3b1d9049cbd46b5e3caf7d4af18da3d852a44e71
Desablero <desablero@gmail.com>
2020-09-09 00:39:11 +0300

    Backup новой версии. Далее откат к старой

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Подключаем модули и создаём бота
 const Telegraf = require('telegraf')
-const config = require('/logs/config')
-const SupportBot = new Telegraf(config.botToken)
+const config = require('./logs/config')
+const fs = require('fs')
+const SupportBot = new Telegraf('1273972291:AAEBdrllJkUGi_SQXvPGpJWY8lKZ5ORTDBY')
 
 // Заготовки текста
 let replyText = {
@@ -13,9 +14,23 @@ let replyText = {
 
 // Проверяем пользователя на права администратора
 let isAdmin = (userId) => {
-    return userId == config.admin;
+    return userId == config.admin1;
 };
 
+
+// Старт бота + GIF
+SupportBot.start((msg) => {
+    const dogGif = new Promise(function (resolve, reject) {
+        msg.telegram.sendDocument(msg.from.id, {
+            source: __dirname + '/src/call_center.gif', // ?????
+            filename: 'call_center.gif'
+        })
+
+    })
+    msg.reply(isAdmin(msg.message.from.id) ? replyText.helloAdmin : (replyText.helloUser1 + replyText.helloUser2))
+    // dogGif.then()
+
+})
 
 // Перенаправляем админу от пользователя или уведомляем админа об ошибке
 let forwardToAdmin = (msg) => {
@@ -23,35 +38,105 @@ let forwardToAdmin = (msg) => {
 
         msg.reply(replyText.replyWrong);
     } else {
-        msg.forwardMessage(config.adminList(1||2), msg.from.id, msg.message.id);
+        msg.forwardMessage(config.admin1, msg.from.id, msg.message.id);
     }
 };
 
-// Старт бота
-bot.start((msg) => {
-    const dogGif = new Promise(function (resolve, reject) {
-        msg.telegram.sendDocument(msg.from.id, {
-            source: __dirname + '/src/call_center.gif', // ?????
-            filename: 'call_center.gif'
-        })
-    })
-    msg.reply(isAdmin(msg.message.from.id) ? replyText.helloAdmin : replyText.helloUser1 + replyText.helloUser2)
-    dogGif.then()
-})
-
 // Слушаем на наличие объекта message
-bot.on('message', (msg) => {
+SupportBot.on('message', (msg) => {
     // убеждаемся что это админ ответил на сообщение пользователя
     if (msg.message.reply_to_message
         && msg.message.reply_to_message.forward_from
         && isAdmin(msg.message.from.id)) {
-        // отправляем копию пользователю
-        msg.telegram.sendCopy(msg.message.reply_to_message.forward_from.id, msg.message);
+        let userId = msg.message.reply_to_message.forward_from.id;
+        //  проверяем что пришло и отправляем соответствующим методом
+        switch (msg.updateSubTypes[0]) {
+            case 'text':
+                msg.telegram.sendMessage(
+                    userId,
+                    msg.message.text
+                );
+                break;
+            case 'sticker':
+                msg.telegram.sendSticker(
+                    userId,
+                    msg.message.sticker.file_id
+                );
+                break;
+            case 'photo':
+                let file = msg.message.photo.length - 1;
+                msg.telegram.sendPhoto(
+                    userId,
+                    msg.message.photo[file].file_id,
+                    {
+                        'caption': msg.message.caption
+                    }
+                );
+                break;
+            case 'document':
+                msg.telegram.sendDocument(
+                    userId,
+                    msg.message.document.file_id,
+                    {
+                        'caption': msg.message.caption
+                    }
+                );
+                break;
+            case 'voice':
+                msg.telegram.sendVoice(
+                    userId,
+                    msg.message.voice.file_id,
+                    {
+                        'caption': msg.message.caption
+                    }
+                );
+                break;
+            case 'video_note':
+                msg.telegram.sendVideoNote(
+                    userId,
+                    msg.message.video_note.file_id
+                );
+                break;
+            case 'video':
+                msg.telegram.sendVideo(
+                    userId,
+                    msg.message.video.file_id,
+                    {
+                        'caption': msg.message.caption
+                    }
+                );
+                break;
+            case 'audio':
+                msg.telegram.sendAudio(
+                    userId,
+                    msg.message.audio.file_id,
+                    {
+                        'caption': msg.message.caption
+                    }
+                );
+                break;
+            default:
+                console.log('other');
+        }
     } else {
-        // перенаправляем админу
         forwardToAdmin(msg);
     }
 });
 
+// function startTime() {
+//     var date = new Date();
+//     var hours = date.getHours();
+//     var minutes = date.getMinutes();
+//     var seconds = date.getSeconds();
+//     if (hours < 10) hours = "0" + hours;
+//     if (minutes < 10) minutes = "0" + minutes;
+//     if (seconds < 10) seconds = "0" + seconds;
+//     console.log(hours + ":" + minutes + ":" + seconds)
+//     setTimeout(startTime, 1000);
+// }
+//
+// startTime()
+
+
 // запуск бота
-bot.launch();
+SupportBot.launch();
--- logs/config.js
+++ logs/config.js
@@ -1,4 +1,6 @@
 module.export = {
     botToken: '1273972291:AAEBdrllJkUGi_SQXvPGpJWY8lKZ5ORTDBY',
-    adminList: ['208562435','769627999']
+    adminList: ['208562435','769627999'],
+    admin1: '208562435',
+    admin2: '769627999'
 }
